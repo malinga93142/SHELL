fd66efc6f9820586f97532f1a98367aa80db78f0
malinga93142 <malingas023@gmail.com>
2023-09-11 09:49:15 +0530

    updated

---
+++ shell/CMakeFiles/LINES.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LINES.dir/lines.c.o"
+  "CMakeFiles/LINES.dir/lines.c.o.d"
+  "LINES"
+  "LINES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LINES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shell/CMakeFiles/LINES.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LINES.
+# This may be replaced when dependencies are built.
